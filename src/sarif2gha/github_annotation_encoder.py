e3a950353c5e9374d504dd04ec30b0f796b2b463
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-04-06 21:04:29 +0900

    feat: Create single-line GitHub Annotation string from internal data
    Closes #5

---
+++ src/sarif2gha/github_annotation_encoder.py
@@ -0,0 +1,68 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Copyright (C) 2026 Minoru Sekine
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+import re
+
+from sarif2gha.analysis_result import AnalysisResult, Severity
+
+
+class GitHubAnnotationEncoder:
+    """Encoder for GitHub Annotation style string from analysis data."""
+    def encode(self, analysis_result: AnalysisResult) -> str:
+        """Encode GitHub Annotation string from AnalysisResult instance."""
+        encoded_str = f"::{self._encode_severity(analysis_result.severity)} "
+        encoded_str += f"file={self._encode_property_str(analysis_result.file)}"
+        if analysis_result.start_line is not None:
+            encoded_str += f",line={analysis_result.start_line}"
+        if analysis_result.start_column is not None:
+            encoded_str += f",col={analysis_result.start_column}"
+        if analysis_result.end_line is not None:
+            encoded_str += f",endLine={analysis_result.end_line}"
+        if analysis_result.end_column is not None:
+            encoded_str += f",endColumn={analysis_result.end_column}"
+        if analysis_result.title is not None:
+            encoded_str += f",title={self._encode_property_str(analysis_result.title)}"
+        encoded_str += f"::{self._encode_data_str(analysis_result.message)}"
+        return encoded_str
+
+    def _encode_severity(self, severity:Severity):
+        """Encode severity enum into GitHub Annotation string."""
+        SEVERITY_STRINGIFY_DICT = {
+            Severity.ERROR: 'error',
+            Severity.WARNING: 'warning',
+            Severity.NOTICE: 'notice',
+            Severity.UNKNOWN: 'warning'
+        }
+        # Fallback default severity is 'warning'.
+        return SEVERITY_STRINGIFY_DICT.get(severity, 'warning')
+
+    def _encode_property_str(self, src:str) -> str:
+        """Encode as property string of GitHub workflow."""
+        PROPERTY_ESCAEPE_DICT = {
+            '%': '%25',
+            '\n': '%0A',
+            '\r': '%0D',
+            ',': '%2C',
+            ':': '%3A'
+        }
+        return self._escape_by_dict(src, PROPERTY_ESCAEPE_DICT)
+
+    def _encode_data_str(self, src:str) -> str:
+        """Encode as property string of GitHub workflow."""
+        PROPERTY_ESCAEPE_DICT = {
+            '%': '%25',
+            '\n': '%0A',
+            '\r': '%0D',
+        }
+        return self._escape_by_dict(src, PROPERTY_ESCAEPE_DICT)
+
+    def _escape_by_dict(self, src: str, escape_dict) -> str:
+        """Escape given str by escape_dict."""
+        re_obj = re.compile("|".join(re.escape(key) for key in escape_dict))
+        return re_obj.sub(lambda m: escape_dict[m.group(0)], src)
---
+++ tests/test_github_annotation_encoder.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Copyright (C) 2026 Minoru Sekine
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+import pytest
+
+from sarif2gha.analysis_result import AnalysisResult, Severity
+from sarif2gha.github_annotation_encoder import GitHubAnnotationEncoder
+
+
+@pytest.fixture()
+def encoder():
+    """Construct test target encoder instance."""
+    return GitHubAnnotationEncoder()
+
+@pytest.mark.parametrize(
+    "analysis_result, expected_str",
+    [
+        pytest.param(
+            AnalysisResult(
+                file='src/foo.py',
+                severity=Severity.WARNING,
+                start_line=7,
+                start_column=2,
+                end_line=8,
+                end_column=79,
+                title='Warning title',
+                message='Main message'
+            ),
+            '::warning '
+            'file=src/foo.py,line=7,col=2,endLine=8,endColumn=79,'
+            'title=Warning title'
+            '::Main message',
+            id='full_parameters'
+        ),
+        pytest.param(
+            AnalysisResult(
+                file='Introduction/simple-example.js',
+                severity=Severity.ERROR,
+                start_line=0,
+                start_column=4,
+                end_line=None,
+                end_column=None,
+                title='disallow unused variables',
+                message="'x' is assigned a value but never used."
+            ),
+            "::error "
+            "file=Introduction/simple-example.js,line=0,col=4,"
+            "title=disallow unused variables"
+            "::'x' is assigned a value but never used.",
+            id='lack_ends'
+        )
+    ]
+)
+
+def test_github_annotation_encoder(encoder, analysis_result, expected_str):
+    """Parameterized tests for typical usages of GitHubAnnotationEncoder."""
+    encoded_str = encoder.encode(analysis_result)
+    assert encoded_str == expected_str
