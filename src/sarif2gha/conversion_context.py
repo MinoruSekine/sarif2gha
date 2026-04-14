4d0f6278882ba35d7183a82c7ca2d373259e326f
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2026-04-14 21:10:21 +0900

    feat: ConversionContext class. (#12)
    Closes #12

---
+++ src/sarif2gha/conversion_context.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# Copyright (C) 2026 Minoru Sekine
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+from pathlib import Path
+
+from sarif2gha.analysis_result import LoadFailureData, LoadSuccessData
+
+
+class ConversionContext:
+    """Context to convert analysis input to output."""
+    _sarif_path: Path
+
+    def __init__(self, sarif_path: Path, loader, encoder, writer):
+        """Contructor."""
+        self._sarif_path = sarif_path
+        self._loader = loader
+        self._encoder = encoder
+        self._writer = writer
+
+    def run(self) -> str | None:
+        """Run conversion."""
+        load_data = self._loader.load(self._sarif_path)
+        if isinstance(load_data, LoadSuccessData):
+            for i in load_data.results:
+                self._writer.write(self._encoder.encode(i))
+            return None
+        if isinstance(load_data, LoadFailureData):
+            return load_data.message
+        raise ValueError("Unknown return value from loader.")
